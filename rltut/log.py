e6558dd9d028e3c9b83f86a0b2d05a6451e9dd75
Douglas Orr <douglas.orr99@gmail.com>
2018-02-14 07:33:16 +0000

    Update logger - should be easier to use & more flexible

--- rltut/log.py
+++ rltut/log.py
@@ -2,60 +2,130 @@ import os
 import re
 import time
 import numpy as np
+import shutil
+import gym
+import json
+import pandas as pd
+import seaborn as sns
 
 
-def _open_log(root, name):
-    '''Open a log file (text) in the given root directory,
-    with an optional name.
+def create_directory(name='out', autoincrement=False, replace=True):
+    '''Create a directory for logging results.
 
-    If name is specified, the file is called "{root}/{name}.log",
-    which is overwritten if it already exists.
+    name -- str -- base name
 
-    If name is None, the file is given a unique incrementing integer
-    name i.e. {root}/0.log, {root}/1.log, etc.
+    replace -- bool -- if True, replace an existing directory
+
+    autoincrement -- bool -- if True, append an autoincrementing number to the
+    directory name
+
+    returns -- str -- created directory
     '''
-    if not os.path.isdir(root):
-        os.makedirs(root)
+    if autoincrement:
+        parent, fname = os.path.split(name)
+        parent = '.' if parent == '' else parent
+        pattern = re.compile(fname + r'(\d+)')
+        if os.path.exists(parent):
+            matches = [pattern.match(f) for f in os.listdir(parent)]
+            n = max(
+                (1 + int(m.group(1)) for m in matches if m is not None),
+                default=0)
+        else:
+            n = 0
+        name = os.path.join(parent, '{}{}'.format(fname, n))
 
-    if name is not None:
-        return open('{}/{}.log'.format(root, name))
+    if os.path.exists(name):
+        if replace:
+            shutil.rmtree(name)
+        else:
+            raise FileExistsError(
+                ('Directory {} already exists - pleas specify either'
+                 ' autoincrement=True or replace=True').format(name))
 
-    pattern = re.compile(r'(\d+)\.log')
-    matches = [pattern.match(f) for f in os.listdir(root)
-               if os.path.isfile(os.path.join(root, f))]
-    next_log = max(
-        (1 + int(m.group(1)) for m in matches if m is not None),
-        default=0)
-    return open('{}/{}.log'.format(root, next_log), 'w')
+    os.makedirs(name)
+    return name
 
 
-class Log:
-    def __init__(self, env, print_every=15, root='logs', name=None):
-        self._env = env
-        self._print_every = print_every
+# Logging
+
+class PrintLogger:
+    def log(self, key, value):
+        value = float(value.data if hasattr(value, 'data') else value)
+        print('{}: {}'.format(key, value))
+
+
+class JsonLogger:
+    def __init__(self, path):
+        self._log = open(path, 'w')
         self._t0 = time.time()
-        self._last_print = dict(time=self._t0, episode=env.episode_id)
-        self._log = _open_log(root, name)
-
-    def __repr__(self):
-        return 'Log({})'.format(self._log.name)
 
     @property
-    def name(self):
+    def path(self):
         return self._log.name
 
-    def tick(self):
+    def log(self, key, value):
+        value = float(value.data if hasattr(value, 'data') else value)
+        self._log.write('{}\n'.format(
+            json.dumps(dict(
+                t=time.time() - self._t0,
+                key=key,
+                value=float(value)))))
+        self._log.flush()
+
+
+def load(f):
+    with open('../out/log.jsonl') as f:
+        return pd.DataFrame.from_dict([json.loads(line) for line in f])
+
+
+def show(f):
+    return sns.lmplot(
+        data=load(f),
+        x='t', y='value', row='key',
+        fit_reg=False, size=5, aspect=2, markers='.', sharey=False)
+
+
+# Monitoring
+
+class Monitor(gym.wrappers.Monitor):
+    '''Helper wrapper to make it easy to watch progress in stdout & in the
+    log.
+    '''
+    def __init__(self, env, directory, print_every=10, **base_args):
+        super().__init__(env, directory=directory, **base_args)
+        self._print_every = print_every
+        self._t0 = time.time()
+        self._last_print = dict(time=self._t0, episode=0)
+        self.logger = JsonLogger(os.path.join(self.directory, 'log.jsonl'))
+        self._logged = 0
+
+    def log(self, key, value):
+        return self.logger.log(key, value)
+
+    def _after_reset(self, observation):
+        super()._after_reset(observation)
+
+        lengths = self.get_episode_lengths()
+        if self._logged < len(lengths):
+            rewards = self.get_episode_rewards()
+            for i in range(self._logged, len(lengths)):
+                self.logger.log('episode.length', lengths[i])
+            for i in range(self._logged, len(rewards)):
+                self.logger.log('episode.reward', rewards[i])
+            self._logged = len(lengths)
+
         now = time.time()
         if (self._print_every is not None and
                 self._print_every < now - self._last_print['time']):
             e0 = self._last_print['episode']
-            print(('t: {time:>3.0f} s'
-                   '  episodes: {episodes:<5}'
-                   '  av-reward: {reward:>5.3g}'
-                   '  av-length: {steps:>5.3g}').format(
-                       time=now - self._t0,
-                       episodes='{}-{}'.format(e0, self._env.episode_id),
-                       reward=np.mean(self._env.get_episode_rewards()[e0:]),
-                       steps=np.mean(self._env.get_episode_lengths()[e0:]),
+            print(('t: {time:<5}'
+                   '  episodes: {episodes:<7}'
+                   '  av-reward: {reward:<4.3g}'
+                   '  av-length: {steps:<4.3g}').format(
+                       time='{:.0f} s'.format(now - self._t0),
+                       episodes='{}-{}'.format(e0, self.episode_id),
+                       reward=np.mean(self.get_episode_rewards()[e0:]),
+                       steps=np.mean(self.get_episode_lengths()[e0:]),
                    ))
-            self._last_print = dict(time=now, episode=self._env.episode_id)
+            self._last_print = dict(
+                time=now, episode=len(self.get_episode_lengths()))
