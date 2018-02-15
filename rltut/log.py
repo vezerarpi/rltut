6f32b66586374caea2a7849b8994079f4ea35961
Douglas Orr <douglas.orr99@gmail.com>
2018-02-15 13:53:12 +0000

    Improve plotting & add videos

--- rltut/log.py
+++ rltut/log.py
@@ -1,12 +1,15 @@
 import os
 import re
 import time
-import numpy as np
+import glob
+import json
 import shutil
 import gym
-import json
+import numpy as np
+import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import IPython.display
 
 
 def create_directory(name='out', autoincrement=False, replace=True):
@@ -46,6 +49,33 @@ def create_directory(name='out', autoincrement=False, replace=True):
     return name
 
 
+# Videos
+
+def find_videos(path=''):
+    '''Return a list of videos.
+    '''
+    return glob.glob(os.path.join(path, '**/*.mp4'), recursive=True)
+
+
+def video(path):
+    '''Show a video from a full path (or find the latest video in a directory).
+
+    path -- string -- path to mp4 file, or a parent directory
+    '''
+    if os.path.isdir(path):
+        # Find the latest video
+        videos = find_videos(path)
+        if len(videos) == 0:
+            raise ValueError('No videos found in directory "{path}"'.format(
+                path=path))
+        path = max(videos, key=os.path.getmtime)
+    subpath = os.path.relpath(os.path.abspath(path), os.getenv('PWD'))
+    return IPython.display.HTML("""
+    <video src="/notebooks/{subpath}" controls autoplay></video>
+    <p>{subpath}</p>
+    """.format(subpath=subpath))
+
+
 # Log recoding
 
 class PrintLogger:
@@ -79,53 +109,144 @@ class JsonLogger:
 
 # Log loading/plotting
 
-def load(name):
-    '''Load a log file as a Pandas DataFrame.
+def _strip_common(files):
+    '''Returns 'files' with common prefix/suffix stripped.
     '''
-    with open(name) as f:
-        return pd.DataFrame.from_dict([json.loads(line) for line in f])
+    if len(files) <= 1:
+        return files
+    start = next(
+        idx for idx, chs in enumerate(zip(*files))
+        if any(ch != chs[0] for ch in chs))
+    end = next(
+        idx for idx, chs in enumerate(zip(*(f[::-1] for f in files)))
+        if any(ch != chs[0] for ch in chs))
+    return [f[start:] if end == 0 else f[start:-end] for f in files]
 
 
-def plot(data, nsmooth=None, point_alpha=None, ax=None):
-    '''Plot datapoints & smoothed curve for elements of d.
+def load(*names_or_patterns, only_match=None):
+    '''Load log file(s) as a Pandas DataFrame.
+
+    names_or_patterns -- string -- path to file, or glob pattern
+
+    only_match -- string or None -- regex to filter unique names by
+
+    returns -- pd.DataFrame
     '''
+    files = [f for p in names_or_patterns
+             for f in glob.glob(p, recursive=True)]
+    dfs = []
+    for path, name in zip(files, _strip_common(files)):
+        if only_match is None or re.search(only_match, name):
+            with open(path) as f:
+                dfs.append(pd.DataFrame.from_dict(
+                    [json.loads(line) for line in f]))
+                dfs[-1]['name'] = name
+    return pd.concat(dfs)[['name', 'key', 't', 'value']]
+
+
+def _autorange_y(ys, tail=0.05):
+    '''Autorange the y axis based on the given ys.
+
+    ys -- pd.Series
+
+    tail -- float -- proportion of y to exclude
+
+    returns -- (float, float) -- preferred range
+    '''
+    return (ys.quantile(tail, interpolation='lower'),
+            ys.quantile(1 - tail, interpolation='higher'))
+
+
+def plot(data, nsmooth=None, point_alpha=None, color=None, ax=None):
+    '''Plot datapoints & smoothed curve for elements of data.
+
+    data -- pd.DataFrame -- plots the 'value' column
+
+    nsmooth -- int -- smoothing window to apply (or 1 to disable)
+
+    point_alpha -- float -- opacity of points (or 0 to disable points)
+
+    color -- tuple -- as from sns.color_palette
+
+    ax -- plt.axes -- e.g. from gca()
+    '''
+    if ax is None:
+        ax = plt.figure().gca()
     if nsmooth is None:
         nsmooth = int(len(data) / 50)
     if point_alpha is None:
-        point_alpha = max(0.05, 1 / (1 + len(data) / 2000))
-    if ax is None:
-        ax = plt.figure().gca()
+        point_alpha = max(0.01, 1 / (1 + len(data) / 1000))
     if nsmooth <= 1:
-        data.plot(x='t', y='value', ax=ax)
+        data.plot(x='t', y='value', color=color, ax=ax)
     else:
         if 0 < point_alpha:
             data.plot(x='t', y='value', marker='.', kind='scatter',
-                      alpha=point_alpha, ax=ax)
+                      alpha=point_alpha, color=color, ax=ax)
         y = data['value'].rolling(
             min_periods=0, window=2 * nsmooth, win_type='gaussian'
         ).mean(std=nsmooth)
-        plt.plot(data['t'], y)
-        d = 0.1 * (y.max() - y.min())
-        plt.ylim((y.min() - d, y.max() + d))
+        plt.plot(data['t'], y, color=color)
+        ax.set_ylim(_autorange_y(y, tail=0))
     plt.ylabel('')
     plt.xlabel('t')
 
 
-def show(f, show_points=True):
+def plot_many(data, colors=None, nsmooth=None, point_alpha=None, ax=None):
+    '''Plot datapoints & smoothed curve for elements of data.
+
+    data -- pd.DataFrame -- plots the 'value' column, grouped by 'name'
+
+    colors -- dict(string -> tuple) -- mapping names to colors
+
+    nsmooth -- int -- smoothing window to apply (or 1 to disable)
+
+    point_alpha -- float -- opacity of points (or 0 to disable points)
+
+    ax -- plt.axes -- e.g. from gca()
+    '''
+    if ax is None:
+        ax = plt.figure().gca()
+    names = sorted(set(data.name))
+    for name in names:
+        plot(data[data.name == name],
+             nsmooth=nsmooth,
+             point_alpha=point_alpha,
+             color=None if colors is None else colors[name],
+             ax=ax)
+    ranges_min, ranges_max = zip(*list(
+        _autorange_y(data[data.name == name].value)
+        for name in names))
+    ax.set_ylim((min(ranges_min), max(ranges_max)))
+    plt.legend(names)
+
+
+def show(*names_or_patterns, show_points=True, keys=None, logs=None):
     '''High level helper to display all traces from a log file.
+
+    show_points -- bool -- draw semitransparent points?
+
+    keys -- string -- pattern to match keys to display
+
+    logs -- string -- pattern to match logs (by name) to display
     '''
     # Simpler version:
     # sns.lmplot(data=load(f), x='t', y='value', row='key',
     #            fit_reg=False, size=5, aspect=2, markers='.', sharey=False)
-    data = load(f)
-    keys = sorted(set(data.key))
+    data = load(*names_or_patterns, only_match=logs)
+    keys = sorted(k for k in set(data.key)
+                  if keys is None or re.search(keys, k))
+    colors = {name: color
+              for name, color in zip(
+                      sorted(set(data.name)),
+                      sns.color_palette('hls', len(set(data.name))))}
     plt.figure(figsize=(10, 6 * len(keys)))
     for row, key in enumerate(keys):
         plt.subplot(len(keys), 1, row + 1)
-        plot(data[data.key == key],
-             point_alpha=(None if show_points else 0),
-             ax=plt.gca())
-        plt.legend([key])
+        plot_many(data[data.key == key],
+                  point_alpha=(None if show_points else 0),
+                  colors=colors,
+                  ax=plt.gca())
+        plt.ylabel(key)
 
 
 # Monitoring
@@ -148,16 +269,14 @@ class Monitor(gym.wrappers.Monitor):
     def _after_reset(self, observation):
         super()._after_reset(observation)
 
-        lengths = self.stats_recorder.episode_lengths
-        if self._logged < len(lengths):
+        rewards = self.stats_recorder.episode_rewards
+        if self._logged < len(rewards):
             TYPEMAP = dict(t='training', e='evaluation')
             types = self.stats_recorder.episode_types
             rewards = self.stats_recorder.episode_rewards
-            for i in range(self._logged, len(lengths)):
-                self.logger.log(TYPEMAP[types[i]] + '.length', lengths[i])
             for i in range(self._logged, len(rewards)):
                 self.logger.log(TYPEMAP[types[i]] + '.reward', rewards[i])
-            self._logged = len(lengths)
+            self._logged = len(rewards)
 
         now = time.time()
         if (self._print_every is not None and
@@ -165,8 +284,7 @@ class Monitor(gym.wrappers.Monitor):
             e0 = self._last_print['episode']
             print(('t: {time:<5}'
                    '  episodes: {episodes:<7}'
-                   '  av-reward: {reward:<4.3g}'
-                   '  av-length: {steps:<4.3g}').format(
+                   '  av-reward: {reward:<4.3g}').format(
                        time='{:.0f} s'.format(now - self._t0),
                        episodes='{}-{}'.format(e0, self.episode_id),
                        reward=np.mean(self.get_episode_rewards()[e0:]),
