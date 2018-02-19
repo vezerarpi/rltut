f1906822fb6f5bd3aca10f3e2a2f023864fe7b9d
Douglas Orr <douglas.orr99@gmail.com>
2018-02-19 12:57:02 +0000

    Fix multi-user server X

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 
 c.Spawner.env_keep = [
-    'PATH', 'PYTHONPATH', 'LANG', 'LC_ALL',
+    'DISPLAY', 'PATH', 'PYTHONPATH', 'LANG', 'LC_ALL',
     'CONDA_ROOT', 'CONDA_DEFAULT_ENV', 'VIRTUAL_ENV',
     'OPENBLAS_NUM_THREADS',
 ]
