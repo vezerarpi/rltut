268c667f24136707e945d4a8ce9ffbb58d36bf4c
Arpi Vezer <arpi.vezer@microsoft.com>
2018-02-22 18:47:53 +0000

    wip more on sharing arrays

--- rltut/examples/atari-a3c.py
+++ rltut/examples/atari-a3c.py
@@ -21,8 +21,9 @@ rng.seed(42)
 
 
 class Model(C.Chain):
-    def __init__(self, observation_shape, n_actions):
+    def __init__(self, observation_shape, n_actions, softmax):
         super().__init__()
+        self._softmax = softmax  # flag for whether to softmax the output
         in_channels = observation_shape[2]
         out_channels1 = 8
         kernel_size1 = 8
@@ -48,7 +49,10 @@ class Model(C.Chain):
         z1 = C.functions.relu(self.conv1(state))
         z2 = C.functions.relu(self.conv2(z1))
         z3 = C.functions.relu(self.projection(z2))
-        return self.action_values(z3)
+        actions = self.action_values(z3)
+        if self._softmax:
+            actions = C.functions.log_softmax(actions)
+        return actions
 
 
 def print_params(chain):
@@ -102,31 +106,36 @@ class A3CAgent(AsyncAgent):
         self._n_actions = n_actions
         self._dummy_state = dummy_state
         self._exp = []
-        self._policy = Model(observation_shape, n_actions)
+        self._policy = Model(observation_shape, n_actions, softmax=True)
         self._optim_policy = optim_factory()
-        self._value = Model(observation_shape, 1)
+        self._value = Model(observation_shape, 1, softmax=False)
         self._optim_value = optim_factory()
 
     def model_params(self):
         params = [self._policy.namedparams()]
+        print('model_params policy', list(map(operator.itemgetter(0), params)))#XXX
         params.extend(self._value.namedparams())
-        print('model_params', list(map(operator.itemgetter(0), params)))#XXX
+        print('model_params value', list(map(operator.itemgetter(0), params)))#XXX
         return dict(params)
 
     def episode_start(self, init_state):
         self._exps.clear()
         self._exps.append(dict(state=prepare_input(init_state),
                                action=None,
+                               paction=None,
+                               value=None,
                                reward=None,
                                done=False))
 
     def act(self):
+        x = C.Variable(self._prepare_input(self._exps['state']))
+        actions = self._policy(x)
+        action = np.argmax(actions.data)
+        paction = actions[action]
+        value = self._value(x)  # TODO move value calc to backup loop in episode_end?
         if rng.random() < self._epsilon:
             action = rng.choice([0, 1])
-        else:
-            x = self._prepare_input(self._exps['state'])
-            action = np.argmax(self._policy(C.Variable(x)).data)
-        self._exps[-1].update(action=action)
+        self._exps[-1].update(action=action, paction=paction, value=value)
         return action
 
     def step_update(self, reward, next_state, done):
@@ -137,11 +146,19 @@ class A3CAgent(AsyncAgent):
         backwards = reversed(self._exps)
         last = next(backwards)
         reward = 0 if last['done'] else self._value(last['state'])
-        gradients = None
+        loss_policy = C.Variable(np.float32(0.0))
+        loss_value = C.Variable(np.float32(0.0))
         for trans in backwards:
             reward = trans['reward'] + self._gamma * reward
-            # TODO accumulate gradients XXX
-        return gradients
+            loss_policy += trans['paction'] * (reward - trans['value'])
+            loss_value += C.functions.squared_error(reward, trans['value'])
+        self._policy.cleargrads()
+        self._value.cleargrads()
+        loss_policy.backward()
+        self._optim_policy.update()
+        loss_value.backward()
+        self._optim_value.update()
+        return loss_policy, loss_value
 
 
 def setup_signal_handlers(shared_flag):
@@ -154,24 +171,35 @@ def setup_signal_handlers(shared_flag):
 
 
 def setup_shared_params(orig_params):
-
-    shared_params = {}
+    # TODO this could be done with a class of (buffer, np array) instead
+    shared_buffers = {}
+    shared_arrays = {}
     for key, param in orig_params.items():
         assert param.dtype == np.float32
-        shared_params[key] = mp.RawArray('f', param.data.ravel())
-    for key, param in orig_params.items():
-        if key in shared_params:
-            param.data = np.from_buffer(
-                shared_params[key], dtype=param.data.dtype).reshape(param.data.shape)
-            print('sharing param', key, param.data.shape)
-    return shared_params
+        shared_buffers[key] = mp.RawArray('f', param.data.ravel())
+        shared_arrays[key] = \
+            np.from_buffer(shared_buffers[key], dtype=param.data.dtype)
+    return shared_arrays, shared_buffers
 
+
+def pull_from_shared(shared_arrays, local_params):
+    # TODO use this in setup_shared_params
+    for key, param in local_params:
+        assert key in shared_arrays
+        param.data = np.from_buffer(
+            shared_arrays[key], dtype=param.data.dtype).reshape(param.data.shape)
+
+def push_to_shared(shared_arrays, local_params):
+    for key, param in local_params:
+        assert key in shared_arrays
+        shared_arrays[key][:] = param.data
 
 def train_async(agent_func, env_func, optim_func, n_procs):
     # init master agent
     master_agent = agent_func(0, optim_func())#XXX model needs to be forwarded to initialise all params
-    # returns params in a dict
-    master_params = setup_shared_params(master_agent.model_params())#XXX
+    # buffers holds the shared memory RawArrays, params holds the numpy
+    # arrays that reference the buffers and should be to assigned to
+    master_arrays, master_rawarrays = setup_shared_params(master_agent.model_params())
     # move master params to shared arrays
 
     shared_stop_flag = mp.RawValue('B', 0)  # uint8, 0 for go, 1 for stop
